9755831608ba2acbd19cc4ed77efa9cb000a5f17
vitorPr0 <senaivitor899@gmail.com>
2023-08-04 11:21:38 -0300

    Versão final

---
+++ assets/main.js
@@ -0,0 +1,13 @@
+function fundo1(){
+    document.body.style.backgroundColor = 'green';
+    document
+}
+function fundo2(){
+    document.body.style.backgroundColor = 'pink';
+}
+function fundo3(){
+    document.body.style.backgroundColor = 'yellow';
+}
+function fundo4(){
+    document.body.style.backgroundColor = 'aqua';
+}
